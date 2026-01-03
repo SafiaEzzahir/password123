7b671074bfd35d51d8a7057ddf71bd6689ed6c7c
SafiaEzzahir <safia.ezzahir@hotmail.com>
2026-01-03 11:54:49 +0000

    Create main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css'
 
+import PasswordSection from './PasswordSection'
+
 function App() {
   return (
-    <></>
+    <div id='App'>
+      <h1 id='Title'>password123</h1>
+      <div id='MainSection'>
+        <p>enter your password below:</p>
+        <PasswordSection />
+      </div>
+    </div>
   )
 }
 
