3b4b906c8492bcdd51429c82675ad3f090e1a7d1
SafiaEzzahir <safia.ezzahir@hotmail.com>
2026-01-03 16:39:49 +0000

    Check if CurrentPassword contains numbers and special characters

--- src/PasswordSection.jsx
+++ src/PasswordSection.jsx
@@ -1,17 +1,46 @@
 import { useState, useEffect } from 'react'
 
+const NumCharacters = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+const SCharacters = ['!', '@', '#', '£', '€', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', '|', '/', '?', '<', '>', ',', '.', '`', '~', '±', '§']
+
 export default function PasswordSection() {
     const [CharacterNumber, setCharacterNumber] = useState(0)
+    const [ErrorMessage, setErrorMessage] = useState(null)
+
+    function isInString(String, Characters) {
+        for (let Char of Characters) {
+            if (String.includes(Char)) {
+                return true
+            }
+        }
+
+        return false
+    }
 
     function onPasswordInputChanged(e) {
         var CurrentPassword = String(e.target.value)
         setCharacterNumber(CurrentPassword.length)
+        
+        if (CurrentPassword.length >= 30) {
+            setErrorMessage(null)
+        } else {
+            var ContainsSC = isInString(CurrentPassword, SCharacters)
+            var ContainsNum = isInString(CurrentPassword, NumCharacters)
+            if (!ContainsNum) {
+                setErrorMessage("no numbers in password!!")
+            } else if (!ContainsSC) {
+                console.log("here")
+                setErrorMessage("no special characters in password!!")
+            } else {setErrorMessage(null)}
+        }
+
     }
 
     return (
         <div id="PasswordSection">
             <input type="password" name="password" id="PasswordInput" onChange={onPasswordInputChanged}/>
             <p id="CharacterNumberId">{CharacterNumber}</p>
+            <p>{ErrorMessage}</p>
         </div>
     )
 }
